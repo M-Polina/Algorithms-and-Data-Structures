c253e85c63a4fa9bcb81a1b3cf18409792461178
M-Polina <p.marusina.2.9@gmail.com>
2021-09-30 00:00:48 +0300

    adding all Algorithms and Data structures project to git

---
+++ src/com/company/Laboratory2/Merge_sort.java
@@ -0,0 +1,9 @@
+package com.company.Laboratory2;
+
+public class Merge_sort {
+
+    public static void main (String args[]){
+
+        System.out.println("Hello Java!");
+    }
+}
---
+++ src/com/company/Laboratory2/Quick_sort.java
@@ -0,0 +1,8 @@
+package com.company.Laboratory2;
+
+public class Quick_sort {
+    public static void main (String args[]){
+
+        System.out.println("Hello f!");
+    }
+}
